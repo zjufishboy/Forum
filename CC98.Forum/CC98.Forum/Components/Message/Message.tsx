e37925af4b589a5c5f15febe8b7732ca6f3dfaa7
zjufishboy <3170101504@zju.edu.cn>
2020-12-09 03:52:45 +0800

    fix: 修复了消息界面消息点击link组件无法跳转的问题

--- CC98.Forum/CC98.Forum/Components/Message/Message.tsx
+++ CC98.Forum/CC98.Forum/Components/Message/Message.tsx
@@ -8,9 +8,8 @@ import { MessageSetting } from './MessageSetting';
 import * as Utility from '../../Utility';
 import DocumentTitle from '../DocumentTitle';
 import {
-    BrowserRouter as Router,
     Route,
-    NavLink
+    NavLink,
 } from 'react-router-dom';
 import Store from '../../Store';
 
@@ -22,35 +21,38 @@ export class Message extends React.Component<{}, AppState> {
     render() {
         let unreadCount = Store.getState().message;
         return (<div className="message-root">
-                    <DocumentTitle title={`CC98论坛-我的消息`} />
+            <DocumentTitle title={`CC98论坛-我的消息`} />
             <div className="message">
-                <div className="readAllMessage">
-                    <button className="message-message-wPostBtn" onClick={Utility.readAll}>全部标为已读</button>
-                </div>
-                            <div className="message-title">我的消息</div>
-                            <Router>
-                                <div className="message-content">
-                                    <div className="message-nav">
-                            <NavLink to="/message/response"><div id="response" className="nav-div">回复我的<div className="message-counterNav displaynone" id="unreadCount-replyCount1">{unreadCount.replyCount}</div></div></NavLink>
-                            <hr/>
-                            <NavLink to="/message/attme"><div id="attme" className="nav-div">@ 我的<div className="message-counterNav displaynone" id="unreadCount-atCount1">{unreadCount.atCount}</div></div></NavLink>
-                            <hr/>
-                            <NavLink to="/message/system"><div id="system" className="nav-div">系统通知<div className="message-counterNav displaynone" id="unreadCount-systemCount1">{unreadCount.systemCount}</div></div></NavLink>
-                            <hr/>
-                            <NavLink to="/message/message"><div id="message" className="nav-div">我的私信<div className="message-counterNav displaynone" id="unreadCount-messageCount1">{unreadCount.messageCount}</div></div></NavLink>
-                            <hr />
-                            <NavLink to="/message/setting"><div id="setting" className="nav-div">消息设置</div></NavLink>
-                        </div>
-                                    <Route exact path="/message" component={MessageResponse}></Route>
-                                    <Route path="/message/response/:page?" component={MessageResponse} />
-                                    <Route path="/message/attme/:page?" component={MessageAttme} />
-                                    <Route path="/message/system/:page?" component={MessageSystem} />
-                                    <Route path="/message/message/:page?" component={MessageMessage} />
-                                    <Route path="/message/setting" component={MessageSetting} />
-                                </div>
-                            </Router>
+                {
+                    location.pathname!=='/message/setting' && 
+                    <div className="readAllMessage">
+                        <button className="message-message-wPostBtn" onClick={Utility.readAll}>全部标为已读</button>
                     </div>
+                }
+                <div className="message-title">我的消息</div>
+
+                <div className="message-content">
+                    <div className="message-nav">
+                        <NavLink to="/message/response"><div id="response" className="nav-div">回复我的<div className="message-counterNav displaynone" id="unreadCount-replyCount1">{unreadCount.replyCount}</div></div></NavLink>
+                        <hr />
+                        <NavLink to="/message/attme"><div id="attme" className="nav-div">@ 我的<div className="message-counterNav displaynone" id="unreadCount-atCount1">{unreadCount.atCount}</div></div></NavLink>
+                        <hr />
+                        <NavLink to="/message/system"><div id="system" className="nav-div">系统通知<div className="message-counterNav displaynone" id="unreadCount-systemCount1">{unreadCount.systemCount}</div></div></NavLink>
+                        <hr />
+                        <NavLink to="/message/message"><div id="message" className="nav-div">我的私信<div className="message-counterNav displaynone" id="unreadCount-messageCount1">{unreadCount.messageCount}</div></div></NavLink>
+                        <hr />
+                        <NavLink to="/message/setting"><div id="setting" className="nav-div">消息设置</div></NavLink>
+                    </div>
+                    <Route exact path="/message" component={MessageResponse} />
+                    <Route path="/message/response/:page?" component={MessageResponse} />
+                    <Route path="/message/attme/:page?" component={MessageAttme} />
+                    <Route path="/message/system/:page?" component={MessageSystem} />
+                    <Route path="/message/message/:page?" component={MessageMessage} />
+                    <Route path="/message/setting" component={MessageSetting} />
                 </div>
-            );
-	}
+
+            </div>
+        </div>
+        );
+    }
 }
